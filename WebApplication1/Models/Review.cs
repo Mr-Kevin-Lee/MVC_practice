8065ce55e5dc61b09cab3c0323988c56570bf319
Kevin Lee <kevin.lee@experianinteractive.com>
2016-05-16 14:58:40 -0700

    Some content changes on About; Added blank Opinions page

---
+++ WebApplication1/Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Review
+    {
+        public Review() {}
+        public int Rating { get; set; }
+        public string Description { get; set; }
+    }
+
+}
